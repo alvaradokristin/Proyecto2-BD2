3beee7f3bb4aba739aa4609491808242abb57cb4
Josué MR <josuemartinez.cr@gmail.com>
2023-05-11 00:11:12 -0600

    Prevista UI

---
+++ Backend/MongoDB/DB/database.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const startConnection = async function() {
+    try{
+        await mongoose.connect('mongodb+srv://josuemartinezcr:bd21234@bd2-p2.d37xqum.mongodb.net/?retryWrites=true&w=majority');
+        console.log("DATABASE CONNECTED"); 
+    } catch(error){
+        console.log("Conexión fallida: ", error)
+    }
+}
+module.exports = { startConnection };
