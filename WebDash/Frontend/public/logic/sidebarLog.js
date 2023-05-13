a1f3ab5836a9e72b92db71988c525f51c096e13f
Josué MR <josuemartinez.cr@gmail.com>
2023-05-13 02:14:20 -0600

    sidebar dinamico

--- WebDash/Frontend/public/logic/sidebarLog.js
+++
@@ -1,7 +0,0 @@
-
-const toggleSidebar = document.getElementById('toggle-sidebar');
-const sidebar = document.getElementById('sidebar');
-
-toggleSidebar.addEventListener('click', function() {
-  sidebar.classList.toggle('show');
-});
